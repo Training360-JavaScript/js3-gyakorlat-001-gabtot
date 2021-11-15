1a15a2f5f9510f68983a6c3e74aadb5bc4ca27a0
gabtot <gabor.toth.de@digikabel.hu>
2021-11-15 01:07:40 +0100

    kész

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,4 @@
 
+const removeDuplicatesFromArray = array => array.filter( ( value, index, array ) => array.indexOf(value) === index );
 
-
-
-
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray;
